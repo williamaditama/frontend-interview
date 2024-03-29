93362b001d60d034ebb3ae2cd0baae78ff193bdf
Fathan Margono <fmgonoo@gmail.com>
2024-03-29 09:52:24 +0700

    feat: fetch data from api to show the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Col, Container, Row,  } from 'react-bootstrap';
+import { fetchList } from './services/api.service';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+interface Task {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+type Tasks = Array<Task>
 
 function App() {
+  const [incompleteTasks, setIncompleteTasks] = React.useState<Tasks>([])
+  const [completedTasks, setCompletedTasks] = React.useState<Tasks>([])
+  
+  useEffect(() => {
+    const getList = async () => {
+      const res = await fetchList()
+      if (res.data.length > 0) {
+        const incompleteTask = res.data.filter((d: any) =>d.completed === false)
+        const completedTask = res.data.filter((d: any) => d.completed)
+        
+        setIncompleteTasks(incompleteTask)
+        setCompletedTasks(completedTask)
+      }
+    }
+
+    getList()
+  }, [])
   return (
-    <div className='bg-red-500'>
-      <h1>TEst</h1>
+    <div className="p-20 grid grid-cols-2 gap-2">
+      <div className="flex flex-col">
+        <h2 className="text-4xl font-medium">Incomplete</h2>
+        
+        {/* wrapper for listing card */}
+        <div className="p-4 flex flex-col gap-2 bg-slate-200">
+          {/* card */}
+          {incompleteTasks.map(task => (
+            <div className="p-4 bg-white rounded">
+              <a href="#" className="text-sm text-slate-600">{task.id}</a>
+              <h4 className="text-lg font-medium capitalize">{task.title} </h4>
+              <p>Removimg blablabla</p>
+            </div>
+          ))}
+          {/* card */}
+        </div>
+        {/* wrapper for listing card */}
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-4xl font-medium">Complete</h2>
+        
+        {/* wrapper for listing card */}
+        <div className="p-4 flex flex-col gap-2 bg-slate-200">
+          {/* card */}
+          {completedTasks.map(task => (
+            <div className="p-4 bg-white rounded">
+              <a href="#" className="text-sm text-slate-600">{task.id}</a>
+              <h4 className="text-lg font-medium capitalize">{task.title} </h4>
+              <p>Removimg blablabla</p>
+            </div>
+          ))}
+          {/* card */}
+        </div>
+        {/* wrapper for listing card */}
+      </div>
     </div>
   );
 }
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export function fetchList() {
+export async function fetchList() {
   // GET https://jsonplaceholder.typicode.com/todos/?_limit=10
+  const res = await axios.get('https://jsonplaceholder.typicode.com/todos/?_limit=10')
+  return res
 }
 
 // Part 2: https://jsonplaceholder.typicode.com/users/{userId}
